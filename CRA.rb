63d0ee3269ba19a7b9b4bff202e1d5a0b2e2ca4d
callunity <heather.e.armstrong@gmail.com>
2014-08-15 16:41:46 -0400

    CRM modify fixed

--- CRA.rb
+++ CRA.rb
@@ -14,6 +14,15 @@ attr_accessor :name
     @rolodex = Rolodex.new
   end
 
+# Main menu loop
+  def main_menu
+    while true
+      print_main_menu
+      input = gets.chomp.to_i
+      call_option(input)
+    end
+  end
+
 # Display main menu
   def print_main_menu
     puts "[1] Add a contact"
@@ -26,16 +35,6 @@ attr_accessor :name
     puts "Choose an option by entering a number:"
   end
 
-# Main menu loop
-  def main_menu
-    while true
-      print_main_menu
-      input = gets.chomp.to_i
-      return if input == 7
-      call_option(input)
-    end
-  end
-
 # Run main menu options
   def call_option(input)
     case input
@@ -43,11 +42,11 @@ attr_accessor :name
       add_contact
     when 2 
       modify_contact
-    when 3 then puts "Display all contacts"
-    when 4 then puts "Display one contact"
-    when 5 then puts "Display a contact's attribute"
-    when 6 then puts "Delete a contact"
-    when 7 then puts "Exit"
+    when 3 then puts "Display all contacts" ## replace with method
+    when 4 then puts "Display one contact" ## replace with method
+    when 5 then puts "Display a contact's attribute" ## replace with method
+    when 6 then puts "Delete a contact" ## replace with method
+    when 7 then return
     else puts "Sorry, that is not an option. Please choose from one of the options listed below:"
     end
   end
@@ -65,63 +64,70 @@ attr_accessor :name
 
       contact = Contact.new(first_name, last_name, email, notes)
       @rolodex.add_contact(contact)
-
-      puts "Added to Rolodex: \n#{contact.contact_display}\n"
+      puts "Added to Rolodex: \n#{contact.contact_display}\n" 
   end
 
 # Modify a contact
   def modify_contact
-    while true
+    while true ## fix end parameter of this loop? figure out order
       puts "What is the ID of the contact you would like to modify?"
-      contact_id = gets.chomp
+      contact_id = gets.chomp.to_i
       contact_to_modify = @rolodex.search(contact_id)
-    # if false main_menu  ## don't want it to call self, so what?
-      modify_menu(contact_to_modify) ## display breaks here
-      contact_attr_to_modify = gets.chomp.to_i
-      modify_contact_action(contact_attr_to_modify)
+      modify_menu(contact_to_modify)
+      modify_contact_action(contact_to_modify)
       puts contact_to_modify.contact_display
     end
   end
 
   def modify_menu(contact)
-    puts "#{contact.contact_display}"
+    puts "#{contact.contact_display}" ## does it recognize it as instance of Contact?
     puts "Would you like to modify this contact? (yes/no)"
     confirm = gets.chomp
     return unless confirm == "yes"
-      
-   
+      ## send up to main menu if no
     puts "Which attribute would you like to modify?"
     puts "[1] First name"
     puts "[2] Last name"
     puts "[3] Email"
     puts "[4] Notes"
     puts "[5] Exit"
-
   end
 
-  def modify_contact_action(input)
-    case input
+  def modify_contact_action(contact)
+    contact_attr_to_modify = gets.chomp.to_i
+    case contact_attr_to_modify
     when 1
       puts "Enter new first name"
       new_first_name = gets.chomp
-      contact_to_modify.first_name = new_first_name
+      contact.first_name = new_first_name
+      ## undefined local variable contact_to_modify 
     when 2
       puts "Enter new last name"
       new_last_name = gets.chomp
-      contact_to_modify.last_name = new_last_name
+      contact.last_name = new_last_name
     when 3
       puts "Enter new email"
       new_email = gets.chomp
-      contact_to_modify.email = new_email
+      contact.email = new_email
     when 4
       puts "Enter new note"
       new_notes = gets.chomp
-      contact_to_modify.notes = new_notes
+      contact.notes = new_notes
     when 5 
       return
     else puts "Sorry, that is not an option."
     end
+    ## figure out how to loop back to enter more values 
+    ## for this contact if desired
   end
+
+  # Display all contacts
+  def display_all_contacts
+    (@rolodex.display("all")).contact_display
+  end
+
+  # Display one contact
+
 
 end
 
--- rolodex.rb
+++ rolodex.rb
@@ -17,7 +17,37 @@ attr_reader :contacts
     @contacts.each do |contact|
       if contact.id == query
         return contact
+      else return true
+        puts "Contact does not exist.\n"
       end
+    end
   end
+
+  def display(number)
+    if number == "all"
+      @contacts.each do |contact|
+        puts contact
+      end
+    else @contacts.each do |contact|
+          if contact.id == number
+            puts contact
+          end
+      end
+    end
+  end
+
 end
-end
+
+
+
+
+
+
+# contact_display broken: inverse order in CRA.rb/69
+# (ie. #{contact} \n Added to Rolodex:) and doesn't run 
+# at all in modify_contact/78
+
+
+## how do I "bump" out of a branch if a certain condition
+## is met? (modify_menu if no — want to return 
+## to modify_contact)
